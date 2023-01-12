b22f6bb356885cd8238a09d2057e93527be6cf4f
jkymonte <jkymonte@gmail.com>
2023-01-12 18:13:22 +0700

    new update skin

--- delcommapp.py
+++ delcommapp.py
@@ -1,12 +1,13 @@
 import shutil
-import subprocess
 import tkinter as tk
 from tkinter import filedialog
 import os
+import customtkinter as Ctk
 
 if not os.path.exists('target'):
     os.mkdir('target')
 def upload_file():
+    text_widget.configure(state='normal')
     filepath = filedialog.askopenfilename()
     print("Uploaded: ", filepath)
     filename = os.path.basename(filepath)
@@ -17,9 +18,10 @@ def upload_file():
     except:
         print("close")
         pass
-
+    text_widget.configure(state='disabled')
     
 def delete_file():
+    text_widget.configure(state='normal')
     filepath = os.getcwd()
     files = os.listdir("target")
     for file in files:
@@ -27,6 +29,7 @@ def delete_file():
         if os.path.isfile(filepath):
             os.remove(filepath)
     text_widget.delete('1.0', tk.END)
+    text_widget.configure(state='disabled')
 
 def download_file():
     folder_selected = filedialog.askdirectory()
@@ -37,20 +40,22 @@ def download_file():
             # menyalin file dari folder "hasil" ke folder yang dipilih
             shutil.copy(file_path, folder_selected)
     download_button.config(state='normal')
-    # clear_button.config(state='normal')
 def generate_file():
-    download_button.config(state='disabled')
-    # clear_button.config(state='disabled')
+    download_button.configure(state='disabled')
+    # clear_button.configure(state='disabled')
     current_folder = os.getcwd()
 
     dirhasil = os.listdir(current_folder+'/target')
     
-    pilihan = (isi1.get('1.0', tk.END))
+    pilihan = isi1.get()
+    print(pilihan)
     # akhir = isi2.get('1.0', tk.END)
     print("\n")
     print("Processing...")
     print("Mohon tunggu...")
     for file in dirhasil:
+        text_widget.configure(state='normal')
+        process.configure(state='normal')
         with open(f'{current_folder}/target/{file}', 'r') as f:
             lines = f.readlines()
         lines_env  = []
@@ -106,57 +111,54 @@ def generate_file():
             print("File "+file+" Done !!!")
             output = "File "+file+" Done !!!"
             process.insert(tk.END, output)
-            download_button.config(state='normal')
+            download_button.configure(state='normal')
             # clear_button.config(state='normal')
         os.remove(f'{current_folder}/target/{file}')
         text_widget.insert('1.0', f'{delfile}-delete {delname}.txt')
-# def clearhasil():
-#     filepath = os.getcwd()
-#     files = os.listdir("hasil")
-#     for file in files:
-#         filepath = os.path.join("hasil", file)
-#         if os.path.isfile(filepath):
-#             os.remove(filepath)
-#     process.delete('1.0', tk.END)
-root = tk.Tk()
+        text_widget.configure(state='disabled')
+        process.configure(state='disabled')
+
+Ctk.set_appearance_mode("Dark")
+Ctk.set_default_color_theme("green")
+
+root = Ctk.CTk()
 root.wm_title("Delete Command App")
 root.wm_iconbitmap("ikon.ico")
-root.geometry("500x500")
-text_widget = tk.Text(root)
-text_widget.config(height=3, width=60)
-upload_button = tk.Button(text="Upload File", command=upload_file)
-upload_button.pack()
+root.geometry("700x600")
+frame = Ctk.CTkFrame(root)
+frame.pack(fill="both", expand=True)
 
-delete_button = tk.Button(text="Delete File", command=delete_file)
+label = Ctk.CTkLabel(frame, text="Delete Command App", font=("Roboto", 24))
+label.pack(pady=12, padx=10)
 
-text_widget.pack()
+text_widget = Ctk.CTkTextbox(frame, state='disabled')
+text_widget.configure(height=60, width=400)
+upload_button = Ctk.CTkButton(frame, text="Upload File", command=upload_file)
+upload_button.pack(pady=12, padx=10)
+
+delete_button = Ctk.CTkButton(frame, text="Delete File", command=delete_file)
 for file in os.listdir("target"):
-    text_widget.insert(tk.END, file + "\n")
-delete_button.pack()
-text_delete = tk.Label(text="Masukan command yang akan dihapus")
-text_delete.pack()
+    print(file)
+    text_widget.configure(state='normal')
+    text_widget.insert("1.0", file+ "\n")
+    text_widget.configure(state='disabled')
+text_widget.pack()
 
-isi1 = tk.Text(root)
-isi1.config(height=2, width=30)
-isi1.pack()
-# text_delete2 = tk.Label(text="Silahkan pilih batas akhir command yang ingin di hapus :")
-# text_delete2.pack()
-isi2 = tk.Text(root)
-isi2.config(height=2, width=30)
-# isi2.pack()
-generate_button = tk.Button(text="Generate", command=generate_file)
-generate_button.pack()
-process = tk.Text(root)
-process.config(height=5, width=60)
-process.pack()
-download_button = tk.Button(root, text="Download Files", command=download_file, state='disabled')
-download_button.pack()
-# clear_button = tk.Button(root, text="Delete All", command=clearhasil, state='disabled')
-# clear_button.pack()
-label = tk.Label(root, text="v1.0.0")
-label.config(font=("Arial", 7))
+delete_button.pack(pady=12, padx=10)
+text_delete = Ctk.CTkLabel(frame, text="Masukan command yang akan dihapus")
+text_delete.pack(pady=12, padx=10)
+
+isi1 = Ctk.CTkEntry(frame)
+isi1.pack(pady=12, padx=10)
+generate_button = Ctk.CTkButton(frame, text="Generate", command=generate_file)
+generate_button.pack(pady=12, padx=10)
+process = Ctk.CTkTextbox(frame, state='disabled')
+process.configure(height=60, width=400)
+process.pack(pady=12, padx=10)
+download_button = Ctk.CTkButton(frame, text="Download Files", command=download_file, state='disabled')
+download_button.pack(pady=12, padx=10)
+label = Ctk.CTkLabel(frame, text="v1.0.0")
 label.place(relx=1, rely=1, x=0, y=0, anchor='se')
-label2 = tk.Label(root, text="Copyright (c) 2023 - By Team Devnet Nusantara Compnet Integrator")
-label2.config(font=("Arial", 8))
+label2 = Ctk.CTkLabel(frame, text="Copyright (c) 2023 - By Jeky")
 label2.place(relx=0, rely=1, x=0, y=0, anchor='sw')
 root.mainloop()
